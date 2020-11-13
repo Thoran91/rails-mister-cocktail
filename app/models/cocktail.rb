3acf5f9a83e33d0c247f8c71c08cefbe820844a9
Tom Horan <tomhoran91@gmail.com>
2020-11-13 15:18:53 +0100

    mister cocktail bare essentials

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,7 +2,8 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :delete_all
   has_many :ingredients, through: :doses
 
+  has_one_attached :photo
+
   validates :name, presence: true
   validates :name, uniqueness: true
-  
 end
