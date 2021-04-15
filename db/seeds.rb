937cf9b3b2b766252ddec3d22deeaf56ab99b228
Tom Horan <tomhoran91@gmail.com>
2021-04-15 13:47:41 +0200

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,23 @@
 puts "cleaning database"
 Ingredient.destroy_all
 
-Ingredient.create(name: "orange juice")
-Ingredient.create(name: "tomato juice")
-Ingredient.create(name: "lemon juice")
+Ingredient.create(name: "Orange Juice")
+Ingredient.create(name: "Tomato Juice")
+Ingredient.create(name: "Lemon Juice")
 
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "whiskey")
-Ingredient.create(name: "rum")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Whiskey")
+Ingredient.create(name: "Rum")
 
-Ingredient.create(name: "ice")
-Ingredient.create(name: "crushed ice")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Crushed Ice")
 
-Ingredient.create(name: "mint leaf")
+Ingredient.create(name: "Mint Leaf")
 
-Ingredient.create(name: "sugar")
-Ingredient.create(name: "egg yolk")
+Ingredient.create(name: "Sugar")
+Ingredient.create(name: "Simple Syrup")
+
+Ingredient.create(name: "Egg Yolk")
+Ingredient.create(name: "Egg White")
 
 puts "finished database"
