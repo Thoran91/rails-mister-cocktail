d4cb4237d4de18d253fc023099923bfded793354
Tom Horan <tomhoran91@gmail.com>
2021-04-30 14:25:57 +0200

    Added final ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,20 +12,24 @@ Ingredient.destroy_all
 Ingredient.create(name: "Orange Juice")
 Ingredient.create(name: "Tomato Juice")
 Ingredient.create(name: "Lemon Juice")
+Ingredient.create(name: "Lime Juice")
 
 Ingredient.create(name: "Vodka")
 Ingredient.create(name: "Whiskey")
 Ingredient.create(name: "Rum")
 Ingredient.create(name: "Gin")
+Ingredient.create(name: "Vermouth")
 
 Ingredient.create(name: "Ice")
 Ingredient.create(name: "Crushed Ice")
 
 Ingredient.create(name: "Mint Leaf")
 Ingredient.create(name: "Olive")
+Ingredient.create(name: "Celery")
 
 Ingredient.create(name: "Sugar")
 Ingredient.create(name: "Simple Syrup")
+Ingredient.create(name: "Worcestershire Sauce" )
 
 Ingredient.create(name: "Egg Yolk")
 Ingredient.create(name: "Egg White")
